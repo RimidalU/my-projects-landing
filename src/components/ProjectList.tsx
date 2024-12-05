fd3a572ed242a16e450eb6ef9d2de8eb448e3a92
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-05 22:43:50 +0300

    feat: add base styles to project-list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
+import clsx from 'clsx'
 
 import { IProject } from '@/models/project.model'
 
 interface IProjectProps {
     projects: IProject[]
     route: string
+    className?: string
 }
-function ProjectList({ projects, route }: IProjectProps) {
+function ProjectList({ projects, route, className }: IProjectProps) {
     return (
-        <ul>
+        <ul className={clsx('flex flex-col gap-2', className)}>
             {projects.map((project) => (
                 <li key={project.img}>
                     <Link href={`/${route}/${project.img}`}>
