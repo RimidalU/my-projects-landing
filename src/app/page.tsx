17a3990dea112c7434fe90401d2125af378bb9fa
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-05 16:01:34 +0300

    refactor: move maine and footer to root-layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 import Link from 'next/link'
 
-import Footer from '@/components/Footer'
 import { PROJECTS } from '@/data/projects'
 
 export default function HomePage() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1>My Projects List</h1>
-                <ul>
-                    {PROJECTS.map((project) => (
-                        <li key={project.img}>
-                            <Link href={`/projects/${project.img}`}>
-                                {project.name}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-            <Footer />
-        </div>
+        <>
+            <h1>My Projects List</h1>
+            <ul>
+                {PROJECTS.map((project) => (
+                    <li key={project.img}>
+                        <Link href={`/projects/${project.img}`}>
+                            {project.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import { use } from 'react'
 
-import Footer from '@/components/Footer'
 import { PROJECTS } from '@/data/projects'
 import { PAGE_REVALIDATE, StaticPageProps } from '@/models/common.model'
 
@@ -15,17 +14,14 @@ export default function ProjectPage({ params }: StaticPageProps) {
     const { name, description, github } = use(getProject(params.slug))
 
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1>{name}</h1>
-                <p>{description}</p>
-                {github && (
-                    <Link href={github} target={'_blank'} aria-label={name}>
-                        Project on GitHab Link
-                    </Link>
-                )}
-            </main>
-            <Footer />
-        </div>
+        <>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            {github && (
+                <Link href={github} target={'_blank'} aria-label={name}>
+                    Project on GitHab Link
+                </Link>
+            )}
+        </>
     )
 }
