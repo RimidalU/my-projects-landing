65c1c19666c046cc11f05598bc8a45b7ed0f6449
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-05 16:26:32 +0300

    feat: add to-home link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
 import './globals.css'
+import Link from 'next/link'
+
 import Footer from '@/components/Footer'
 
 const geistSans = localFont({
@@ -32,6 +34,10 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                    <footer>
+                        <Link href={'/'}>To Home</Link>
+                    </footer>
+
                     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                         {children}
                     </main>
