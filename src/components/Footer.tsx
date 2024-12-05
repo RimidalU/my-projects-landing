01948abcaa9b61b17afcc87edb05049e754863bc
uladzimir <stankevichuuladimiru@gmail.com>
2024-12-05 14:29:07 +0300

    feat: implement footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="fixed bottom-0 w-full overflow-hidden text-center bg-backgroundOpacity backdrop-blur-[6px]">
+            <a
+                className="hover:text-accent"
+                href="https://github.com/RimidalU"
+                target="_blank"
+            >
+                RimidalU &copy; 2024
+            </a>
+        </footer>
+    )
+}
+
+export default Footer
